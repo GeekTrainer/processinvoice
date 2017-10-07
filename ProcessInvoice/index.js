a174e4b41f8d652575b956b29065b585bbe7da51
Christopher Harrison <charrison@geektrainer.com>
2017-10-07 18:18:00 +0900

    Testing

--- ProcessInvoice/index.js
+++ ProcessInvoice/index.js
@@ -1,6 +1,6 @@
 module.exports = function (context, input) {
     if (!!input && input.length > 0) {
-        context.log('Document Id: ', input[0]);
+        context.log('Document Id: ', input[0]['$v']['company']['$v']);
     }
 
     context.done();
